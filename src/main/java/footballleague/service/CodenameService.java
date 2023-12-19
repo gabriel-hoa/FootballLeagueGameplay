9bbd4bf9f64d3d15498fa7093fbeaf98a815d103
gabriel-hoa <gabrielhoaraujo100@gmail.com>
2023-12-19 09:09:35 -0300

    Services

--- src/main/java/footballleague/service/CodenameService.java
+++ src/main/java/footballleague/service/CodenameService.java
@@ -1,31 +1,94 @@
 package footballleague.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import jakarta.annotation.PostConstruct;
 import lombok.Getter;
+import org.hibernate.cfg.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Getter
 public class CodenameService {
 
     @Autowired
-    private  RestTemplate restTemplate;
+    private RestTemplate restTemplate;
 
     @Autowired
     private Environment env;
 
     private List<String> laligaCodenameList = new ArrayList<>();
+    private List<String> serieaCodenameList = new ArrayList<>();
+    private List<String> premierleagueCodenameList = new ArrayList<>();
+
 
     ObjectMapper objectMapper = new ObjectMapper();
+
+    @PostConstruct
 
     public void loadJsonData(){
 
         try {
-            String codenameResponse = restTemplate.getForObject(env.getProperty("LA_LIGA"), String.class);
+            String codenameResponse = restTemplate.getForObject(env.getProperty("laliga"), String.class);
+            JsonNode jsonNode = objectMapper.readTree(codenameResponse);
+
+            ArrayNode laliga = (ArrayNode) jsonNode.get("laliga");
+
+            for(JsonNode item: laliga) {
+                this.laligaCodenameList.add(item.get("codename").asText());
+
+            }
+
+
+        } catch(Exception e){
+            e.printStackTrace();
         }
     }
+
+    public void loadJsonData(){
+
+        try {
+            String codenameResponse = restTemplate.getForObject(env.getProperty("seriea"), String.class);
+            JsonNode jsonNode = objectMapper.readTree(codenameResponse);
+
+            ArrayNode seriea = (ArrayNode) jsonNode.get("seriea");
+
+            for(JsonNode item: seriea) {
+                this.serieaCodenameList.add(item.get("codename").asText());
+
+            }
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void loadJsonData(){
+
+        try {
+            String codenameResponse = restTemplate.getForObject(env.getProperty("premierleague"), String.class);
+            JsonNode jsonNode = objectMapper.readTree(codenameResponse);
+
+            ArrayNode premierleague = (ArrayNode) jsonNode.get("premierleague");
+
+            for(JsonNode item: premierleague) {
+                this.premierleagueCodenameList.add(item.get("codename").asText());
+
+            }
+
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+
 
 }
--- src/main/java/footballleague/service/PlayerService.java
+++ src/main/java/footballleague/service/PlayerService.java
@@ -1,5 +1,7 @@
 package footballleague.service;
 
+import footballleague.infra.CodenameHandler;
+import footballleague.model.GroupType;
 import footballleague.model.Player;
 import footballleague.model.PlayerDto;
 import footballleague.repositories.PlayerRepository;
@@ -9,10 +11,21 @@ public class PlayerService {
 
     @Autowired
     private PlayerRepository repository;
+
+    @Autowired
+    private CodenameHandler handler;
+
     public Player createPlayer(PlayerDto dto) {
         Player newPlayer = new Player(dto);
+        String codename = getCodename(dto.groupType());
+        newPlayer.setCodename(codename);
         return repository.save(newPlayer);
 
+    }
+
+    private String getCodename(GroupType groupType) {
+        return handler.findCodename(groupType);
 
     }
+
 }
